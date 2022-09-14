3a468839c1b3ffea3cd74fc179db7ad316907f88
johnnkwoemezie <nkwoemeziejohn@gmail.com>
2022-09-13 20:54:09 -0700

    nested whli loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - Prints the alphabet 10 times.
-*
-* Return: Always 0
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ * Return: Always 0 (Success)
 */
-
 void print_alphabet_x10(void)
 {
-	int i, j;
+	int a, b;
 
-	for (i = 0; i <= 9; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 97; j <= 122; j++)
+		for (b = 97; b <= 122; b++)
 		{
-			_putchar(j);
+			_putchar(b);
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
